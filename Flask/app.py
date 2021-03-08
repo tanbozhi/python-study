6db436b16a426d0619a3692660959565fcb8cadb
tbz <1548638666@tbz.com>
2021-03-09 00:38:01 +0800

    flask

--- Flask/app.py
+++ Flask/app.py
@@ -5,4 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index/index.html")
+
+@app.route("/log")
+def login():
+    return render_template("index/login.html")
+
+@app.route("/reg")
+def register():
+    return render_template("index/register.html")
